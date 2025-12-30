b41727c40703900d134ca8dcffbd02e924a5bcdc
Heshan <heshansandaruwan999@gmail.com>
2025-12-30 19:12:16 +0530

    auth provider add

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,17 +1,35 @@
-import { useState } from "react";
+import { use, useState } from "react";
+import { AuthContext } from "./AuthContext";
 
 interface AuthProviderProps {
   children: React.ReactNode;
 }
 
 export const AuthProvider = ({ children }: AuthProviderProps) => {
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
-    const [accessToken, setAccessToken] = useState<string>("");
-    const [isAuthLoading, setIsAuthLoading] = useState<boolean>(true);
-    const [role, setRole] = useState<string>("user");
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+  const [accessToken, setAccessToken] = useState<string>("");
+  const [isAuthenticating, setIsAuthenticating] = useState<boolean>(true);
+  const [role, setRole] = useState<string>("user");
+
+  const login = (token: string, userRole: string) => {
+    setIsLoggedIn(true);
+    setAccessToken(token);
+    setRole(userRole);
+    
+  }
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setAccessToken("");
+    setRole("user");
+  }
+
+  
 
 
-    return (
-        
-    )
+  return (
+    <AuthContext.Provider value={{ isLoggedIn, login, logout, isAuthenticating, role}}>
+      {children}
+      </AuthContext.Provider>
+  )
 }
