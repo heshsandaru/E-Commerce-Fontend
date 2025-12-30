14660dcce17da5674c0ed7f91bf72f7c7ca5521c
Heshan <heshansandaruwan999@gmail.com>
2025-12-30 22:42:43 +0530

    loging page add

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 
 import { AuthProvider } from './context/AuthProvider.tsx'
+import {  RouterProvider } from 'react-router-dom'
+import router from './router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-
+      <RouterProvider router={router} />
     </AuthProvider>
   </StrictMode>
 )
